02e7364e5c13218f9490de348639beb5482bebe4
ajinkyawani-unh <60481083+ajinkyawani-unh@users.noreply.github.com>
2020-02-13 19:50:20 -0500

    Initial Commit

---
+++ PizzaHub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PizzaHub
+//
+//  Created by Ajinkya Wani on 2/13/20.
+//  Copyright © 2020 Ajinkya Wani. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
